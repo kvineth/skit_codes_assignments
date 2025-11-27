dfe77a238d5c974c91ccd23993726e4785f7c622
Admin <Admin@DESKTOP-12I76GO>
2025-11-27 22:00:21 +0530

    First commit

---
+++ Assignments/src/Addition/Add.java
@@ -0,0 +1,10 @@
+package Addition;
+
+public class Add 
+{
+	int a,b;
+	public static int Add(int a,int b)
+	{
+		return a+b;
+	}
+}
---
+++ Assignments/src/Test/Test.java
@@ -0,0 +1,11 @@
+package Test;
+import Addition.Add;
+
+public class Test 
+{	
+	public static void main(String[] args) 
+	{
+		int a=Add.Add(10,20);
+		System.out.println(a);
+	}
+}
