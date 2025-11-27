394bdde8fcb6230138d0c94f626f1a1c5c664a38
Admin <Admin@DESKTOP-12I76GO>
2025-11-27 20:58:15 +0530

    First commit

---
+++ Assignments/src/Polymor/Calcul.java
@@ -0,0 +1,27 @@
+package Polymor;
+import java.util.Scanner;
+
+public class Calcul {
+
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Overloaded method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Calcul myCalculator = new Calcul();
+
+        // Calling the add method with two arguments
+        int sumTwoNumbers = myCalculator.add(5, 10);
+        System.out.println("Sum of two numbers: " + sumTwoNumbers);
+
+        // Calling the add method with three arguments
+        int sumThreeNumbers = myCalculator.add(5, 10, 15);
+        System.out.println("Sum of three numbers: " + sumThreeNumbers);
+    }
+}
