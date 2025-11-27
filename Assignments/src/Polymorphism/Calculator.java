5beba17fb7b312500938025f96a4ab374943cc5d
Admin <Admin@DESKTOP-12I76GO>
2025-11-27 20:37:43 +0530

    First commit

---
+++ Assignments/src/Polymorphism/Calculator.java
@@ -0,0 +1,26 @@
+package Polymorphism;
+
+public class Calculator {
+
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Overloaded method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+
+        // Calling the add method with two arguments
+        int sumTwoNumbers = myCalculator.add(5, 10);
+        System.out.println("Sum of two numbers: " + sumTwoNumbers);
+
+        // Calling the add method with three arguments
+        int sumThreeNumbers = myCalculator.add(5, 10, 15);
+        System.out.println("Sum of three numbers: " + sumThreeNumbers);
+    }
+}
